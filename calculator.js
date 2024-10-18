a514f86e4a2eaa8d2724d2325738b119d80ead21
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-18 19:32:13 -0400

    create basic math functions

---
+++ calculator.js
@@ -0,0 +1,18 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+function subtractNumbers(a, b) {
+    return a - b;
+}
+
+function divideNumbers(a, b) {
+    return a / b;
+}
+
+function multiplyNumbers(a, b) {
+    return a * b
+}
+
