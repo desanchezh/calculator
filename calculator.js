23b687c4f204d0023bda2e1056fb7435fdb43a89
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-18 19:52:25 -0400

    Create operate function and variable placeholders

--- calculator.js
+++ calculator.js
@@ -1,4 +1,11 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const operators = ['+', '-', '/', '*'
+]
+let firstNumber = []
+let secondNumber = []
+let chosenOperator = []
+chosenOperator.push(operators[3])
+result = []
 
 function addNumbers(a, b) {
     return a + b;
@@ -16,3 +23,21 @@ function multiplyNumbers(a, b) {
     return a * b
 }
 
+function operate(a, b, operator){
+    result.length = 0
+    switch(operator){
+        case '+':
+            result.push(addNumbers(a, b));
+            break;
+        case '-':
+            result.push(subtractNumbers(a, b));
+            break;
+        case '/':
+            result.push(divideNumbers(a, b));
+            break;
+        case '*':
+            result.push(multiplyNumbers(a, b));
+            break;
+    }
+
+}
