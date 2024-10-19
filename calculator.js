b8d47588f10735650c47413f4bf220aa8f59bdc3
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-18 20:39:55 -0400

    fix operate function

--- calculator.js
+++ calculator.js
@@ -1,11 +1,12 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const operators = ['+', '-', '/', '*'
-]
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+const operators = ['+', '-', '/', '*']
 let firstNumber = []
 let secondNumber = []
 let chosenOperator = []
-chosenOperator.push(operators[3])
-result = []
+chosenOperator.push(operators[2])
+firstNumber.push(numbers[1],[0],[0])
+secondNumber.push(numbers[6])
+let result = []
 
 function addNumbers(a, b) {
     return a + b;
@@ -28,16 +29,20 @@ function operate(a, b, operator){
     switch(operator){
         case '+':
             result.push(addNumbers(a, b));
-            break;
+            return result;
         case '-':
             result.push(subtractNumbers(a, b));
-            break;
+            return result;
         case '/':
             result.push(divideNumbers(a, b));
-            break;
+            return result;
         case '*':
             result.push(multiplyNumbers(a, b));
-            break;
+            return result;
     }
 
 }
+
+console.log(operate(Number(firstNumber.join('')),
+                    Number(secondNumber.join('')), 
+                    chosenOperator.join('')))
