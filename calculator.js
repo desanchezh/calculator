5db133f26ad05dc241130acd5292a0c42d04871b
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-21 20:35:55 -0400

    Hold operator buttons down and minor fixes

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-//improvement ideas...STOP SCROLL, change operator buttons so they remain visually pressed until = is pressed, delete button, keyboard input
+//improvement ideas... change operator buttons so they remain visually pressed until = is pressed, delete button, keyboard input
 
 let firstNumber = [];
 let secondNumber = [];
@@ -6,6 +6,7 @@ let chosenOperator = [];
 let result = [];
 let numberPressed = []
 let displayContainer = document.querySelector('#displayContainer');
+let currentOperatorGlobal = ''; //this used to store the previous operator to clear out tranform (holding down button effect)
 
 function addNumbers(a, b) {
     return a + b;
@@ -23,7 +24,7 @@ function multiplyNumbers(a, b) {
     return a * b
 }
 
-//decide which function to use and  push result to array
+
 function operate(a, b, operator){
     result.length = 0
     switch(operator){
@@ -33,19 +34,25 @@ function operate(a, b, operator){
         case '-':
             result.push(subtractNumbers(a, b));
             return result;
-        case '/':
+        case '÷'://bad idea using text value as case...this looks like a plus
             result.push(divideNumbers(a, b));
             return result;
-        case '*':
+        case 'x':
             result.push(multiplyNumbers(a, b));
             return result;
     }
 }
 
-//format array for calc
 function cleanArray(numberArr) {
     let clean = Number(numberArr.join(''));
     return clean;
+}
+
+function removeOperatorTransform() {
+  if (!currentOperatorGlobal == '') {
+    currentOperatorGlobal.style.transform = '';
+    currentOperatorGlobal.style.background = '';
+  }
 }
 
 //shows array as string in calculator display
@@ -53,18 +60,22 @@ function getDisplayNum(arr) {
     let displayNum = document.createElement('span');
     displayContainer.appendChild(displayNum);
     displayNum.textContent = +cleanArray(arr).toFixed(2);
-    displayNum.style.color = "white"
+    displayNum.style.background = ''
+    displayNum.style.color = 'white'
 }
 
 let btnNum = document.querySelector('#numberContainer');
 btnNum.addEventListener('click',function (e) {
-    if (e.target.id == 'clear') {//checks for clear
+    if (e.target.id == 'clear') {
         firstNumber.length = 0;
         secondNumber.length = 0;
         chosenOperator.length = 0;
         result.length = 0;
         numberPressed.length = 0;
         displayContainer.textContent = ''
+        document.querySelector('button').style.transform = null
+        removeOperatorTransform();
+
     }else if (chosenOperator.length == 0) {//checks if operator is already selected
         result.length = 0
         numberPressed.length = 0;
@@ -73,7 +84,7 @@ btnNum.addEventListener('click',function (e) {
         displayContainer.textContent = ''
         getDisplayNum(firstNumber);
     }
-    else if (chosenOperator.Length != 0){ //operator not selected yet
+    else if (chosenOperator.Length != 0){ 
         numberPressed.length = 0;
         numberPressed.push(e.target.id);
         secondNumber.push(cleanArray(numberPressed));
@@ -83,32 +94,37 @@ btnNum.addEventListener('click',function (e) {
 })
 
 
-
 let btnOperator = document.querySelector('#operatorContainer');
 btnOperator.addEventListener('click', function (e) {
+  removeOperatorTransform();
+  console.log(currentOperatorGlobal)
     if (result.length == 0){ //operator not  selected yet
-        displayContainer.textContent = ''
         chosenOperator.length = 0;
-        chosenOperator.push(e.target.id)
-        //add some effect here to show current selection (pressed down in css?)
+        selection = e.target.textContent
+        chosenOperator.push(selection)
     }else if (result.length != 0) {
         chosenOperator.length = 0;
-        chosenOperator.push(e.target.id)
+        chosenOperator.push(selection)
     }
+  let currentOperator = document.querySelector(`#${e.target.id}`);
+  currentOperator.style.transform = "translateY(-2px)";
+  currentOperator.style.background = "#B0002C"
+  currentOperatorGlobal = currentOperator;
+  
 })
 
 
 let btnSpecial = document.querySelector('#specialContainer');
 btnSpecial.addEventListener('click',function (e) {
     //don't try to divide by zero
-    if (e.target.id == '=' && cleanArray(secondNumber) == 0 && chosenOperator.join('') == '/') {
-        displayContainer.textContent = 'get a grip';
+    removeOperatorTransform();
+    if (e.target.id == '=' && cleanArray(secondNumber) == 0 && chosenOperator.join('') == '÷') {
+        displayContainer.textContent = ">:(____";
         displayContainer.style.color = 'white';
         firstNumber.length = 0;
         secondNumber.length = 0;
         chosenOperator.length = 0;
         result.length = 0;
-
     }
     else if (e.target.id == '=' && result.length != 0) { //check if result is stored  and sets as first num if so
         operate(cleanArray(result), cleanArray(secondNumber), chosenOperator.join(''))
@@ -117,6 +133,7 @@ btnSpecial.addEventListener('click',function (e) {
         chosenOperator.length = 0
         firstNumber.length = 0
         secondNumber.length = 0
+        removeOperatorTransform();
     }else if (e.target.id == '=') { 
             operate(cleanArray(firstNumber), cleanArray(secondNumber), chosenOperator.join(''))//standard calculation
             displayContainer.textContent = ''
